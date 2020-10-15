1a6903cfb46d90c0a3c747c669defa456ad899dd
TheBigBushman <alanbushell91@gmail.com>
2020-10-15 20:40:04 +0000

    Completed login and logout functionality

--- app.py
+++ app.py
@@ -84,6 +84,19 @@ def profile(username):
         {"username": session["user"]})["username"]
     return render_template("profile.html", username=username)
 
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # logs out user
+    flash("You have been successfully logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
